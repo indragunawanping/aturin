14407b800cb3c71bc3c3f1cbcf18d3cfc5ff0b1a
Indra Gunawan <indragunawanxiaopingping@gmmail.com>
2020-10-02 10:21:33 +0700

    Add assets icon-logo and add logo to header.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
+    path: 'aturin',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'aturin',
     pathMatch: 'full'
   },
 ];
